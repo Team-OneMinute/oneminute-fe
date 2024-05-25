98eb2d1e785ea9ff700e09dbc8a4563d58afed31
shuta <sdsds.com@hotmail.co.jp>
2024-05-26 04:15:26 +0900

    add request parameters when request check hash function.

--- src/app/hooks/useAuthConnect.ts
+++ src/app/hooks/useAuthConnect.ts
@@ -3,8 +3,10 @@ export function useAuthConnect() {
     firebaseAuthConnect: (initData: string) => {
       const tmpString =
         "OneMinute query_id=AAHWOmBqAAAAANY6YGqe3sSw&user=%7B%22id%22%3A1784691414%2C%22first_name%22%3A%22Shuta%22%2C%22last_name%22%3A%22Iwasaki%22%2C%22username%22%3A%22shoo5123%22%2C%22language_code%22%3A%22ja%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1716644843&hash=88c8a828344c45685c2ee9e3ce804748863b6924cf562b312fb530340cc90193";
+      const tmpString2 =
+        "query_id=AAHWOmBqAAAAANY6YGqe3sSw&user=%7B%22id%22%3A1784691414%2C%22first_name%22%3A%22Shuta%22%2C%22last_name%22%3A%22Iwasaki%22%2C%22username%22%3A%22shoo5123%22%2C%22language_code%22%3A%22ja%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1716644843&hash=88c8a828344c45685c2ee9e3ce804748863b6924cf562b312fb530340cc90193";
 
-      const decodedInitData = decodeURIComponent(tmpString);
+      const decodedInitData = decodeURIComponent(tmpString2);
       console.log(decodedInitData);
 
       // Step 2: リクエストパラメータを配列に格納
@@ -24,7 +26,9 @@ export function useAuthConnect() {
       // STEP:4 Firebase functionで　hash検証
 
       // Step5:Firebase auth connect
-      console.log(requestParam);
+          console.log(requestParam);
+          
+          
       return true;
       //     firebase
       //       .auth()
