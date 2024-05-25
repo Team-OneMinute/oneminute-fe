2c5fc1139521286900a1638f6e3e267c99fde916
shuta <sdsds.com@hotmail.co.jp>
2024-05-25 17:14:26 +0900

    add telegram CDN script for get initDat when coming miniapp

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -4,63 +4,55 @@ import firebase from "firebase/compat/app";
 import "firebase/auth";
 import { firebaseConfig } from "@/app/config/firebaseConfig";
 
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-} else {
-  firebase.app(); // if already initialized, use that one
-}
+// if (!firebase.apps.length) {
+//   firebase.initializeApp(firebaseConfig);
+// } else {
+//   firebase.app(); // if already initialized, use that one
+// }
 
-interface User {
-  displayName: string | null;
-}
+// interface User {
+//   displayName: string | null;
+// }
 
 export default function Login() {
-  const [user, setUser] = useState<User | null>(null);
+  //const [user, setUser] = useState<User | null>(null);
+  const [initData, setInitData] = useState<string>("initialString");
 
   useEffect(() => {
-    // URLからクエリパラメータを取得
-    const urlParams = new URLSearchParams(window.location.search);
-    const token = urlParams.get("token");
+    // // URLからクエリパラメータを取得
+    // const urlParams = new URLSearchParams(window.location.search);
+    // const token = urlParams.get("token");
 
-    if (token) {
-      // カスタムトークンを使用してFirebaseにログイン
-      firebase
-        .auth()
-        .signInWithCustomToken(token)
-        .then((userCredential) => {
-          console.log("User logged in:", userCredential.user);
-          setUser(userCredential.user as User);
-        })
-        .catch((error) => {
-          console.error("Error signing in with custom token:", error);
-        });
-    }
+    // if (token) {
+    //   // カスタムトークンを使用してFirebaseにログイン
+    //   firebase
+    //     .auth()
+    //     .signInWithCustomToken(token)
+    //     .then((userCredential) => {
+    //       console.log("User logged in:", userCredential.user);
+    //       setUser(userCredential.user as User);
+    //     })
+    //     .catch((error) => {
+    //       console.error("Error signing in with custom token:", error);
+    //     });
+    // }
+    const script = document.createElement("script");
+    script.src = "https://telegram.org/js/telegram-web-app.js";
+    script.async = true;
+    script.onload = () => {
+      if (window.Telegram) {
+        window.Telegram.WebApp.ready();
+        const initData = window.Telegram.WebApp.initData;
+        setInitData(initData);
+        console.log(initData);
+      }
+    };
+    document.head.appendChild(script);
+
+    return () => {
+      document.head.removeChild(script);
+    };
   }, []);
 
-  if (!user) {
-    return <div>Loading...</div>;
-  }
-
-  return (
-    <div className="App">
-      <h1>Login with Telegram</h1>
-      {!user ? (
-        <div>
-          <div
-            className="telegram-login"
-            data-telegram-login="@OneMInuteStgbot"
-            data-size="large"
-            data-auth-url="https://us-central1-oneminute-88837.cloudfunctions.net/telegramAuth"
-            data-request-access="write"
-          ></div>
-          <script
-            async
-            src="https://telegram.org/js/telegram-widget.js?7"
-          ></script>
-        </div>
-      ) : (
-        <h1>Welcome, {user.displayName || "User"}!</h1>
-      )}
-    </div>
-  );
+  return <div>login page & init dat = {initData}</div>;
 }
