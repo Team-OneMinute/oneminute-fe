a4f28829e2657e534d58c986a937f4bdfa410220
shuta <sdsds.com@hotmail.co.jp>
2024-05-29 21:26:39 +0900

    add firestore hooks test.

---
+++ src/app/test/infrastructure/use-firestore.spec.ts
@@ -0,0 +1,9 @@
+/**
+ * @jest-environment jsdom
+ */
+import { renderHook, act } from "@testing-library/react";
+describe("useFirestore", () => {
+  test("add document", () => {
+    // empty logics, because void simple logic.
+  });
+});
