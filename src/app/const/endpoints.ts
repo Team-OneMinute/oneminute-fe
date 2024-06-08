cbda2c319dbb83183d17005b43e81e5fe51f34a7
takeumanorekishi <takeumanorekishi@gmail.com>
2024-06-09 01:29:15 +0900

    update endpoint

--- src/app/const/endpoints.ts
+++ src/app/const/endpoints.ts
@@ -1,2 +1,2 @@
-export const TELEGRAM_AUTH = "/app_name-v1-presentation-telegramAuth";
-export const START_MINI_GAME_BY_LIFE = "app_name-v1-presentation-startMiniGameByLife";
+export const TELEGRAM_AUTH = "/app-v1-presentation-telegramAuth";
+export const START_MINI_GAME_BY_LIFE = "app-v1-presentation-startMiniGameByLife";
