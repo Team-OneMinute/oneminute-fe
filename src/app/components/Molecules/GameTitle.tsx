603309e21cb0cb016953cbf53af980417d65421d
shuta <sdsds.com@hotmail.co.jp>
2024-06-20 23:54:17 +0900

    refactor welcomeLoading

--- src/app/components/Molecules/GameTitle.tsx
+++ src/app/components/Molecules/GameTitle.tsx
@@ -1,4 +1,3 @@
-import styled from "styled-components";
 import { Text } from "@/app/components/Atoms/Text";
 
 interface Props {
@@ -9,13 +8,6 @@ export const GameTitle = (props: Props) => {
   const { text } = props;
 
   return (
-    <Titlediv>
       <Text text={text} size="xlarge" color="white" />
-    </Titlediv>
   );
 };
-
-const Titlediv = styled.div`
-  height: 100%;
-  width: 100%;
-`;
