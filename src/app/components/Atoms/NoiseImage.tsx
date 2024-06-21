0f32fbddf3f6a58281c408fc4e5e5e77e4b84a77
shuta <sdsds.com@hotmail.co.jp>
2024-06-21 15:05:36 +0900

    fix gridge animation

--- src/app/components/Atoms/NoiseImage.tsx
+++ src/app/components/Atoms/NoiseImage.tsx
@@ -1,85 +1,55 @@
 // components/atoms/AnimationImage.tsx
 import React, { useEffect, useRef } from "react";
-import styled, { keyframes } from "styled-components";
-
-export const filmNoise = keyframes`
-  0%, 100% {
-    filter: none;
-  }
-  20%, 40%, 60%, 80% {
-    filter: contrast(1.5) brightness(1.5) saturate(1.5);
-  }
-`;
-
-export const distort1 = keyframes`
-  0%, 100% {
-    transform: none;
-  }
-  50% {
-    transform: skewX(30deg) skewY(30deg);
-  }
-`;
-
-export const distort2 = keyframes`
-  0%, 100% {
-    transform: none;
-  }
-  50% {
-    transform: skewX(-30deg) skewY(-30deg);
-  }
-`;
+import styled from "styled-components";
+import { filmNoise, glitch } from "@/app/components/animation/animations";
 
 const RotatingImage = styled.img`
   width: 100%;
   height: 100%;
 
   &.noise {
-    animation: ${filmNoise} 0.5s infinite;
+    animation: ${filmNoise} 1s infinite;
   }
 
-  &.distort1 {
-    animation: ${distort1} 0.1s linear;
-  }
-
-  &.distort2 {
-    animation: ${distort2} 0.1s linear;
+  &.glitch {
+    animation: ${glitch} 0.3s infinite;
   }
 `;
 
 export const NoiseImage: React.FC<{ src: string }> = ({ src }) => {
-  const imageRef = useRef<HTMLImageElement>(null);
+const imageRef = useRef<HTMLImageElement>(null);
 
-  const applyEffect = (effect: string) => {
-    if (imageRef.current) {
-      imageRef.current.classList.add(effect);
-      setTimeout(() => {
-        if (imageRef.current) {
-          imageRef.current.classList.remove(effect);
-        }
-      }, 100);
-    }
-  };
-
-  const randomEffects = () => {
-    setInterval(() => {
-      applyEffect("noise");
-
-      if (Math.random() > 0.5) {
-        const distortEffect = Math.random() > 0.5 ? "distort1" : "distort2";
-        setTimeout(() => {
-          applyEffect(distortEffect);
-        }, 100);
+const applyEffect = (effect: string) => {
+  if (imageRef.current) {
+    imageRef.current.classList.add(effect);
+    setTimeout(() => {
+      if (imageRef.current) {
+        imageRef.current.classList.remove(effect);
       }
-    }, Math.random() * 5000 + 1000);
-  };
+    }, 200); // エフェクトの持続時間を設定
+  }
+};
 
-  useEffect(() => {
-    randomEffects();
-  }, []);
+const randomEffects = () => {
+  setInterval(() => {
+    applyEffect("noise");
 
-  return (
-    <div>
-      <RotatingImage src={src} ref={imageRef} />
-    </div>
-  );
+    // ランダムでグリッチエフェクトを適用
+    if (Math.random() > 0.3) {
+      setTimeout(() => {
+        applyEffect("glitch");
+      }, 200); // ノイズのエフェクト後にグリッチを適用
+    }
+  }, Math.random() * 3000 + 2000); // 2秒から10秒の間隔でノイズエフェクトを適用
+};
+
+useEffect(() => {
+  randomEffects();
+}, []);
+
+return (
+  <div>
+    <RotatingImage src={src} ref={imageRef} />
+  </div>
+);
 };
