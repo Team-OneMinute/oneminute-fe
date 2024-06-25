53d46f6df2e9ef7ae1d50feba3e35176e2c0a539
takeumanorekishi <takeumanorekishi@gmail.com>
2024-06-25 11:26:37 +0900

    refactor game design

--- src/app/(pages)/game/page.tsx
+++ src/app/(pages)/game/page.tsx
@@ -5,6 +5,7 @@ import * as THREE from "three";
 import { useCustomEffect } from "@/app/hooks/infrastructure/useCustomEffect";
 import { usePageNavigate } from "@/app/hooks/util/usePageNavigate";
 import { useGameFinish } from "@/app/hooks/service/useGameFinish";
+import styled from "styled-components";
 
 export default function Game() {
   const [result, setResult] = useState<string | null>(null);
@@ -17,7 +18,7 @@ export default function Game() {
     const randomNumber = Math.floor(Math.random() * 20000);
     setRandomNum(randomNumber);
     setResult(randomNumber > 10000 ? "You Win!" : "You Lose.");
-    await finishGame("00001", randomNumber).then((message) => {
+    await finishGame("0001", randomNumber).then((message) => {
       setMessage(message);
     });
   };
@@ -75,17 +76,17 @@ export default function Game() {
         height: "100vh",
         backgroundColor: "black",
         color: "white",
-        padding: "20px",
+        // padding: "4vw",
       }}
     >
-      <Button
+      {/* <Button
         variant="contained"
         sx={{ marginBottom: "20px" }}
         onClick={handleStartClick}
       >
         START
-      </Button>
-      {result && (
+      </Button> */}
+      {/* {result && (
         <>
           <Button
             variant="contained"
@@ -105,9 +106,16 @@ export default function Game() {
           <Typography variant="h4">{`randomNum: ${randomNum}`}</Typography>
           <Typography variant="h4">{`message: ${message}`}</Typography>
         </>
-      )}
+      )} */}
 
       <Box id="game-canvas" sx={{ width: "100%", height: "100%" }}></Box>
+      {/* <GameCanvas id="game-canvas" /> */}
     </Box>
   );
 }
+
+const GameCanvas = styled.div`
+  display: block;
+  height: 100%;
+  width: 100%;
+`;
