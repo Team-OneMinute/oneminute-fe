1e81896369b6c4ce3ab5a1e20c5a9468408490cd
shuta <sdsds.com@hotmail.co.jp>
2024-07-01 19:18:40 +0900

    refactor haptic feedback

--- src/app/slides/InviteSlide.tsx
+++ src/app/slides/InviteSlide.tsx
@@ -2,20 +2,9 @@
 import { SubTitle } from "@/app/components/Molecules/SubTitle";
 
 export const InviteSlide = () => {
-  const hapticHandler = (
-    action: "light" | "medium" | "heavy" | "rigid" | "soft"
-  ) => {
-    window.Telegram.WebApp.HapticFeedback.impactOccurred(action);
-  };
-
   return (
     <>
-      <button onClick={() => hapticHandler("light")}>light</button>
-      <button onClick={() => hapticHandler("medium")}>medium</button>
-      <button onClick={() => hapticHandler("heavy")}>heavy</button>
-      <button onClick={() => hapticHandler("rigid")}>rigid</button>
-      <button onClick={() => hapticHandler("soft")}>soft</button>
-      {/* <SubTitle text="Coming Soon..." /> */}
+      <SubTitle text="Coming Soon..." />
     </>
   );
 };
